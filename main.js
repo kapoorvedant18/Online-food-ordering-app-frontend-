6bce9d1cc44bd4559b6de4567d63904915bad7b3
Vedant Kapur <kapoorvedant18@gmail.com>
2026-02-25 21:06:56 +0530

    more work in main.js but stuck in implementing app ui change

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let i = 0;
+let cart = [];
+
 let initialization = function() {
     fetch('https://dummyjson.com/recipes').
         then(response => response.json()).then(data => {
@@ -5,7 +8,36 @@ let initialization = function() {
                 let item = document.createElement("p");
                 item.innerHTML = items.name;
                 let image = document.createElement("img");
-                document.getElementById("menu").appendChild(item);
+                image.src = items.image
+                image.style.width = "200px";
+                let addContainer = document.createElement("div");
+                addContainer.id ="add button" + i;
+                let addButton = document.createElement("button");
+                addButton.innerHTML = "Add to Cart";
+                addButton.onclick = ()=> {addToCart(items,i)};
+                addContainer.appendChild(addButton);
+                let itemContainer = document.createElement("div"); 
+                itemContainer.appendChild(image);
+                itemContainer.appendChild(item);
+                itemContainer.appendChild(addContainer);
+                itemContainer.style.display = "flex";
+                document.getElementById("menu").appendChild(itemContainer);
+                i++;
         })
     })  
+}
+
+function addToCart(items,x) {
+    document.getElementsByTagName("button")[x].style.display = "none";
+    let addcounter = document.createElement("p");
+    let plus = document.createElement("button");
+    let minus = document.createElement("button");
+    plus.innerHTML = "+";
+    minus.innerHTML = "-";
+    addcounter.innerHTML = "1";
+    // document.getElementById("add button" + i).appendChild(plus);
+    // document.getElementById("add button" + i).appendChild(addcounter);
+    // document.getElementById("add button" + i).appendChild(minus);
+    cart.push(x);
+    console.log(cart);
 }

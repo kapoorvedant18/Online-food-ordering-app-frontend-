e701db9d747c5ce4996a25c0983f3831fa72a3b2
Vedant Kapur <kapoorvedant18@gmail.com>
2026-04-09 19:24:21 +0530

    cart is fixed and complete consistency between home page and cart is there next is cartpage payment ui

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 let cart = [];
 
 let initialization = function() {
+    cart = JSON.parse(localStorage.getItem("cart")) || [];
+    console.log(cart);
     fetch('https://dummyjson.com/recipes').
         then(response => response.json()).then(data => {
             data.recipes.forEach(items => {
@@ -22,7 +24,7 @@ let initialization = function() {
                 let addButton = document.createElement("button");
                 addButton.innerHTML = "Add to Cart";
                 addButton.id = "addbutton" + items.id;
-                addButton.onclick = ()=> {addToCart(items.id)};
+                addButton.onclick = ()=> {addToCart(items.id,true)};
                 addContainer.appendChild(addButton);
                 let itemContainer = document.createElement("div");
                 itemContainer.id = "itemcontainer"; 
@@ -35,43 +37,68 @@ let initialization = function() {
                 itemContainer.appendChild(addContainer);
                 itemContainer.style.display = "flex";
                 document.getElementById("menu").appendChild(itemContainer);
+                if (cart.find((cartitem) => cartitem.itemid === items.id)) {
+                    addToCart(items.id,false);
+                }
+                // console.log(cart.find((cartitem) => cartitem.itemid === items.id));
         })
-    })  
+    }).
+    catch(error => {
+        if (error.name === 'AbortError') {
+            console.log('Fetch aborted');
+        } else {
+            console.error('Fetch error:', error);
+        }
+    });
+
 }
 
-function addToCart(id) {
+function addToCart(id,push) {
     document.getElementById("addbutton"+id).style.display = "none";
+    if (push === true) {
+    cart.push({itemid : id, quantity : 1,price : 10});
+    console.log(cart);
+    localStorage.setItem("cart", JSON.stringify(cart));
+    }
     let addcounter = document.createElement("p");
     let plus = document.createElement("button");
     let minus = document.createElement("button");
     plus.innerHTML = "+";
     minus.innerHTML = "-";
-    addcounter.innerHTML = "1";
+    addcounter.innerHTML = cart.find((cartitem) => cartitem.itemid === id).quantity;
     document.getElementById(id).appendChild(plus);
     document.getElementById(id).appendChild(addcounter);
     document.getElementById(id).appendChild(minus);
     document.getElementById(id).style.display = "inline-block";
-    cart.push({itemid : id, quantity : 1,price : 10});
     plus.onclick = () => {
         let item = cart.findIndex((item) => item.itemid === id);
         cart[item].quantity++;
         addcounter.innerHTML = cart[item].quantity;
+        localStorage.setItem("cart", JSON.stringify(cart));
+        console.log(cart);
     }
     minus.onclick = () => {
         let item = cart.findIndex((item) => item.itemid === id);
         if (cart[item].quantity > 0) {
             cart[item].quantity--;
             addcounter.innerHTML = cart[item].quantity;
+            localStorage.setItem("cart", JSON.stringify(cart));
+            console.log(cart);
         }
         if (cart[item].quantity === 0) {
             plus.style.display = "none";
             minus.style.display = "none";
             addcounter.style.display = "none";
             document.getElementById("addbutton"+id).style.display = "";
+            cart.splice(item, 1);
+            localStorage.setItem("cart", JSON.stringify(cart));
+            console.log(cart);
         }
     }
 }
-let currentController = null;   
+
+let currentController = null;  
+
 function searchItems() {
     let searchquery = document.getElementById("search").value;
 
@@ -124,37 +151,61 @@ function searchItems() {
 }
 
 function showCart() {
-    localStorage.setItem("cart", JSON.stringify(cart)); 
-    cart.forEach(items => {
+    let cart1 = JSON.parse(localStorage.getItem("cart")) || [];
+
+    cart1.forEach(items => {
         fetch('https://dummyjson.com/recipes/'+items.itemid).then(response => response.json()).
         then(cartitem=>{
                 let item = document.createElement("p");
-                item.innerHTML = cartitem.recipes.name;
+                item.innerHTML = cartitem.name;
                 let image = document.createElement("img");
-                image.src = cartitem.recipes.image
+                image.src = cartitem.image
                 image.style.width = "200px";
                 let price = document.createElement("p");
                 price.innerHTML = "10 $";
                 let tags = document.createElement("p");
-                tags.innerHTML = "Tags : " + cartitem.recipes.tags.join(", ");
+                tags.innerHTML = "Tags : " + cartitem.tags.join(", ");
                 let quantity = document.createElement("p");
                 let plus = document.createElement("button");
                 let minus = document.createElement("button");
                 plus.innerHTML = "+";
+                plus.onclick = () => {
+                    items.quantity++;
+                    quantity.innerHTML = items.quantity;
+                    localStorage.setItem("cart", JSON.stringify(cart1));
+                    console.log(cart1);
+                }
                 minus.innerHTML = "-";
-                quantity.innerHTML =  cartitem.recipes.quantity;
+                minus.onclick= () => {
+                    if (items.quantity >= 1) {
+                        items.quantity--;
+                        quantity.innerHTML = items.quantity;
+                        localStorage.setItem("cart", JSON.stringify(cart1));
+                        console.log(cart1);
+                    }
+                    if (items.quantity === 0) {
+                        const removeelement = document.getElementById("cartitems"+items.itemid);
+                        removeelement.remove();     
+                        cart1.splice(items, 1);
+                        localStorage.setItem("cart", JSON.stringify(cart1));
+                        console.log(cart1);
+                }
+            }
+                quantity.innerHTML =  items.quantity;
                 let cartitemstatus = document.createElement("div");
                 cartitemstatus.id = "cartitemstatus";
                 cartitemstatus.appendChild(plus);
                 cartitemstatus.appendChild(quantity);
                 cartitemstatus.appendChild(minus);
                 let cartitems = document.createElement("div");
-                cartitems.appendChild(item);
+                cartitems.id = "cartitems"+items.itemid;
+                cartitems.style.display = "flex";
                 cartitems.appendChild(image);
+                cartitems.appendChild(item);
                 cartitems.appendChild(price);
                 cartitems.appendChild(tags);
                 cartitems.appendChild(cartitemstatus);
-                document.getElementById("cart_items").appendChild(cartitems);
+                document.getElementById("cart_items").appendChild(cartitems); 
         })
     })
 
